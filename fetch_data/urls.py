6e480b4e623415803f41cfc2bda4897f614f5d84
Chatrasham <aminizadyar6800@gmail.com>
2023-01-01 17:55:20 +0330

    just checking

--- fetch_data/urls.py
+++ fetch_data/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from fetch_data.views import landing_page,fetch_page
+from fetch_data.views import landing_page,fetch_page, clean_page
 
 
 
 urlpatterns = [
     path('', landing_page, name='landing_page'),
     path('fetch/', fetch_page, name='fetch_page'),
+    path('clean/', clean_page, name='clean_page'),
 ]
--- fetch_data/views.py
+++ fetch_data/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from .fetch import do_fetch
+#from sector_slc.settings import BASE_DIR
+#import os
+#import glob
+#industry_indices_path = os.path.join(BASE_DIR,"industry_indices")
+#processed_data_path = os.path.join(BASE_DIR ,"processed_data")
+
 
 def landing_page(request):
     
@@ -10,4 +16,14 @@ def landing_page(request):
 def fetch_page(request):
     do_fetch()
     return render(request, 'fetch.html')
+
+
+#def clean_page(request):
+    #files = glob.glob(industry_indices_path)
+    #for f in files:
+        #os.remove(f)
+    #files = glob.glob(processed_data_path)
+    #for f in files:
+        #os.remove(f)
+    #return render(request, 'clean.html')
 
